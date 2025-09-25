9ecdbc0c738f1906852fbe2fc15aa41b7f4c6d52
klu2300032719 <2300032719@kluniversity.in>
2025-09-25 18:06:20 +0530

    Some changes are made

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2026/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2028/auth",
 });
 
 export default api;
+
