bdc646408b1f84fbe93d903d866811ca2fc67e4b
klu2300032719 <2300032719@kluniversity.in>
2025-11-26 12:41:27 +0530

    changes in mysql

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2028/auth",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2028",
 });
 
 export default api;
-
