d30ed577e6c7a9a2632d3aac4f58f4f8baf6edf2
klu2300032719 <2300032719@kluniversity.in>
2025-11-26 14:23:44 +0530

    changes made

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2028",  // backend running inside Docker
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://springboot:2026",
 });
 
 export default api;
