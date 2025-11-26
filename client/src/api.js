124057c4e675653571e3a5842b6303bafb7b4a18
klu2300032719 <2300032719@kluniversity.in>
2025-11-26 14:05:10 +0530

    some changes

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2028",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://springboot:2026",
 });
 
 export default api;
